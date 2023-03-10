805a9ef88e1572e03af5559e2ff27e761b0fe35f
krobicon <113786742+krobicon@users.noreply.github.com>
2023-03-10 11:13:56 -0500

    Update LocalPlayer.cpp

--- LocalPlayer.cpp
+++ LocalPlayer.cpp
@@ -30,7 +30,7 @@ public:
 	long basePointer = getBasePointer();
 	long ptrLong = basePointer + offsets::WEAPON;
 	ulong wepHandle = mem::ReadLong(ptrLong);
-	wepHandle &= 0xffff;
+	//wepHandle &= 0xffff;
 	return wepHandle;
     }
     long getWeaponEntity()
