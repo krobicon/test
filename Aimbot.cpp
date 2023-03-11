1df6384f169ff68b8e3d20167f9b1e74c48e24f5
krobicon <113786742+krobicon@users.noreply.github.com>
2023-03-11 12:05:06 -0500

    Update Aimbot.cpp

--- Aimbot.cpp
+++ Aimbot.cpp
@@ -118,7 +118,7 @@ public:
 		if (distanceToTarget < 10)
 		{
 			m_x11Utils->mouseClick(1);
-			std::this_thread::sleep_for(std::chrono::milliseconds(50));
+			//std::this_thread::sleep_for(std::chrono::milliseconds(50));
 		}
 	}
     }
