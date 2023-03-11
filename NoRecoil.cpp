26819c144c6047ed9489ab31d8f6cabc27118c97
krobicon <113786742+krobicon@users.noreply.github.com>
2023-03-11 18:50:17 -0500

    Update NoRecoil.cpp

--- NoRecoil.cpp
+++ NoRecoil.cpp
@@ -34,7 +34,8 @@ public:
     }
     void update()
     {
-
+        float weirdorandom = RandomFloat(0.35,0.60);
+        printf("my weirdo random float: [%f] \n", weirdorandom);
         // adjust pitch
         const double norecoilPitchStrength = m_configLoader->getNorecoilPitchStrength();
         const double punchPitch = m_localPlayer->getPunchPitch();
