2bc296ea466b935963953305e145d5f7c65caed1
krobicon <113786742+krobicon@users.noreply.github.com>
2023-03-11 00:36:14 -0500

    Update Aimbot.cpp

--- Aimbot.cpp
+++ Aimbot.cpp
@@ -93,7 +93,7 @@ public:
         const double pitchAngleDeltaAbs = abs(pitchAngleDelta);
 	printf("MY PITCH: [%f] \n", pitch);
 	printf("PITCH DELTA: [%f] \n", pitchAngleDeltaAbs);
-	printf("BOOL RETURN?: [%d] \n", pitchAngleDeltaAbs > m_configLoader->getAimbotActivationFOV() / 2);
+	printf("WHAT IS THIS: [%d] \n", m_configLoader->getAimbotActivationFOV() / 2);
         if (pitchAngleDeltaAbs > m_configLoader->getAimbotActivationFOV() / 2)
             return;
         double newPitch = normalizePitch(pitch + (pitchAngleDelta / m_configLoader->getAimbotSmoothing()));
