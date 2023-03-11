1251e397b89acddae4302dd97092f202e773089d
krobicon <113786742+krobicon@users.noreply.github.com>
2023-03-11 17:38:30 -0500

    Update Aimbot.cpp

--- Aimbot.cpp
+++ Aimbot.cpp
@@ -254,6 +254,8 @@ public:
 	    {
                 continue;
 	    }
+            //if (m_level->isSpecialMode() && player->getTeamSpecial() == m_localPlayer->getTeamSpecial())
+                //continue;
             if (player->getTeamNumber() == m_localPlayer->getTeamNumber())
                 continue;
             if (!player->isVisible(false))
