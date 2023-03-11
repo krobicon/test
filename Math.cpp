c39f5d15d51ce3cef4099fd72eb1767fd5ca67cf
krobicon <113786742+krobicon@users.noreply.github.com>
2023-03-11 18:46:09 -0500

    Update Math.cpp

--- Math.cpp
+++ Math.cpp
@@ -29,5 +29,11 @@ namespace math
         float distance = sqrt(pow(dx, 2) + pow(dy, 2));
         return distance;
     }
-
+    float RandomFloat(float a, float b) 
+    {
+        float random = ((float) rand()) / (float) RAND_MAX;
+        float diff = b - a;
+        float r = random * diff;
+        return a + r;
+    }
 }
