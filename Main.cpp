63b580e234084fb94da1ef22696915f845c9fb98
krobicon <113786742+krobicon@users.noreply.github.com>
2023-03-15 18:07:32 -0400

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -97,6 +97,7 @@ int main(int argc, char *argv[])
             if (counter % 10000 == 0)
             {
                 printf("UPDATE[%d] OK. \n", counter);
+		counter = 1;
             }
             std::this_thread::sleep_for(std::chrono::milliseconds(5));
         }
@@ -106,7 +107,7 @@ int main(int argc, char *argv[])
             std::this_thread::sleep_for(std::chrono::seconds(10));
         }
         counter++;
-        if (counter > 10000)
-            counter = 1;
+        //if (counter > 10000)
+            //counter = 1;
     }
 }
