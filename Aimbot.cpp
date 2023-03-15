5d0d373ad99fa5e60affddce9d18ddfba8451789
krobicon <113786742+krobicon@users.noreply.github.com>
2023-03-14 21:16:22 -0400

    Update Aimbot.cpp

--- Aimbot.cpp
+++ Aimbot.cpp
@@ -109,8 +109,7 @@ public:
 		bool triggerSent = false;
 		smooth = smooth/2;
 		fov = fov*2;
-		//if (m_lockedOnPlayer->isCrosshair() && localWeapon->getReadyTime() == 0)
-		if (localWeapon->getReadyTime() == 0)
+		if (m_lockedOnPlayer->isCrosshair() && localWeapon->getReadyTime() == 0)
 		{
 			m_localPlayer->setAttackState(5);
 			triggerSent = true;
