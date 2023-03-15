4cf24f45bb12154f78b4871897a40c6f7ef84f58
krobicon <113786742+krobicon@users.noreply.github.com>
2023-03-15 01:20:39 -0400

    Update NoRecoil.cpp

--- NoRecoil.cpp
+++ NoRecoil.cpp
@@ -52,7 +52,7 @@ public:
         const double punchYaw = m_localPlayer->getPunchYaw();
         if (punchYaw != 0)
         {
-            const double norecoilYawStrength = math::RandomFloat(0.37,0.60); // RANDOM
+            const double norecoilYawStrength = math::RandomFloat(0.40,0.60); // RANDOM
             const double yaw = m_localPlayer->getYaw();
             const double punchYawDelta = (punchYaw - m_previousPunchYaw) * norecoilYawStrength;
             m_localPlayer->setYaw(yaw - punchYawDelta);
