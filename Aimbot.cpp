146553e287b68825786042a1f19ac174a9dafb98
krobicon <113786742+krobicon@users.noreply.github.com>
2023-03-17 14:44:57 -0400

    Update Aimbot.cpp

--- Aimbot.cpp
+++ Aimbot.cpp
@@ -251,8 +251,8 @@ public:
 	    }
             if (m_level->isSpecialMode() && player->getTeamSpecial() == m_localPlayer->getTeamSpecial())
                 continue;
-            //if (player->getTeamNumber() == m_localPlayer->getTeamNumber())
-                //continue;
+            if (player->getTeamNumber() == m_localPlayer->getTeamNumber())
+                continue;
             if (!player->isVisible(false))
                 continue;
             double desiredViewAngleYaw = calculateDesiredYaw(m_localPlayer->getLocationX(),
