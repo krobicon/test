7c23605b0938d55e4314a38af70a1f049f18c54e
krobicon <113786742+krobicon@users.noreply.github.com>
2023-03-18 11:46:56 -0400

    Update Aimbot.cpp

--- Aimbot.cpp
+++ Aimbot.cpp
@@ -286,9 +286,13 @@ public:
                 continue;
 	    }
             if (m_level->isSpecialMode() && player->getTeamSpecial() == m_localPlayer->getTeamSpecial())
+	    {
                 continue;
-            //if (player->getTeamNumber() == m_localPlayer->getTeamNumber())
-                //continue;
+	    }
+            else if (player->getTeamNumber() == m_localPlayer->getTeamNumber())
+	    {
+                continue;
+	    }
             //if (!player->isVisible(false))
                 //continue;
             double desiredViewAngleYaw = calculateDesiredYaw(m_localPlayer->getLocationX(),
