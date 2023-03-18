f87e1168a75f16145f6daae2e0a6e2409bbb0988
krobicon <113786742+krobicon@users.noreply.github.com>
2023-03-18 10:28:15 -0400

    Update Aimbot.cpp

--- Aimbot.cpp
+++ Aimbot.cpp
@@ -317,7 +317,7 @@ public:
 	    if (result.empty()) {
                 continue;
             }
-	    cout << result;
+	    std::cout << result;
 	    if (!(result.compare("npc_dummie") == 0))
 	    {
 		    continue;
