eaaba2f03e1b36311b3429a1f6f50fdfc5221de7
krobicon <113786742+krobicon@users.noreply.github.com>
2023-03-18 10:22:39 -0400

    Update Aimbot.cpp

--- Aimbot.cpp
+++ Aimbot.cpp
@@ -345,10 +345,10 @@ public:
 
         Player *closestPlayerSoFar = nullptr;
         double closestPlayerAngleSoFar;
-        for (int i = 0; i < npcs.size(); i++)
+        for (int i = 0; i < npcs->size(); i++)
         {
             //printf("i = %d\n", i);
-            Player* player = &(npcs.at(i));
+            Player* player = &(npcs->at(i));
             //if (!c_npc->isValid())
                 //continue;
             //if (c_npc->isDead())
