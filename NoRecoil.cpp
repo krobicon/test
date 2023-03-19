308cd5dc9e47d90f37942b6e5513691b306dc757
krobicon <113786742+krobicon@users.noreply.github.com>
2023-03-19 13:50:53 -0400

    Update NoRecoil.cpp

--- NoRecoil.cpp
+++ NoRecoil.cpp
@@ -1,7 +1,6 @@
 #pragma once
 #include <vector>
 #include "LocalPlayer.cpp"
-#include "Player.cpp"
 #include "Math.cpp"
 #include "Level.cpp"
 #include "X11Utils.cpp"
@@ -11,9 +10,7 @@ class NoRecoil
 {
 private:
     ConfigLoader *m_configLoader;
-    Level *m_level;
     LocalPlayer *m_localPlayer;
-    std::vector<Player *> *m_players;
     X11Utils *m_x11Utils;
 
     double m_previousPunchPitch = 0;
@@ -21,15 +18,11 @@ private:
 
 public:
     NoRecoil(ConfigLoader *configLoader,
-             Level *level,
              LocalPlayer *localPlayer,
-             std::vector<Player *> *players,
              X11Utils *x11Utils)
     {
         m_configLoader = configLoader;
-        m_level = level;
         m_localPlayer = localPlayer;
-        m_players = players;
         m_x11Utils = x11Utils;
     }
     void update()
