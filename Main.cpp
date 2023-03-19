4ccf3e94f43f7c8b654cbf1140e72f5b683cefaf
krobicon <113786742+krobicon@users.noreply.github.com>
2023-03-19 13:42:21 -0400

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -72,7 +72,7 @@ int main(int argc, char *argv[])
 			Weapon *localWeapon = new Weapon(localPlayer->getWeaponHandle());
 			if (configLoader->isAimbotOn())
 			{
-			    aimbot->update(counter, localWeapon);
+			    aimbot->update(localWeapon);
 			}
 
 			noRecoil->update();
