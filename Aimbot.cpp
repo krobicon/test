81d30284430bd9a99b56a4c00433a755569d23e9
krobicon <113786742+krobicon@users.noreply.github.com>
2023-03-20 14:01:50 -0400

    Update Aimbot.cpp

--- Aimbot.cpp
+++ Aimbot.cpp
@@ -125,7 +125,7 @@ public:
 		{
 			smooth = smooth/5;
 			//fov = fov*2;
-			fov = 28 - distanceToTarget * 1.1;
+			fov = 30 - distanceToTarget * 1.15;
 			crosshaired = m_lockedOnPlayer->isCrosshair();
 			readytofire = true;
 		}
