f050d8e44232a7421f5789c3d1ecaac9b1a0e8bf
krobicon <113786742+krobicon@users.noreply.github.com>
2023-03-20 14:20:32 -0400

    Update Aimbot.cpp

--- Aimbot.cpp
+++ Aimbot.cpp
@@ -310,8 +310,8 @@ public:
 	    {
                 continue;
 	    }
-            if (m_level->isSpecialMode() && player->getTeamSpecial() == m_localPlayer->getTeamSpecial())
-                continue;
+            //if (m_level->isSpecialMode() && player->getTeamSpecial() == m_localPlayer->getTeamSpecial())
+                //continue;
             //if (player->getTeamNumber() == m_localPlayer->getTeamNumber())
                 //continue;
 
