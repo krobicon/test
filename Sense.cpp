e181f0c201a3f917ad62d37031025a25b0a0f462
krobicon <113786742+krobicon@users.noreply.github.com>
2023-05-25 16:47:01 -0400

    Update Sense.cpp

--- Sense.cpp
+++ Sense.cpp
@@ -48,7 +48,7 @@ public:
                                                           player->getLocationY(),
                                                           player->getLocationZ());
             
-            if (distanceToTarget < 260)
+            if (distanceToTarget < 270)
             {
                 if (player->isVisible(true))
                 {
