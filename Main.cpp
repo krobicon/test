8ad710a792c21b374a45b9f4e3707f13c14ea0aa
krobicon <113786742+krobicon@users.noreply.github.com>
2023-05-25 14:15:19 -0400

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -70,7 +70,7 @@ int main(int argc, char *argv[])
 		if (!localPlayer->isDead() && !localPlayer->isKnocked())
 		{
 			Weapon *localWeapon = new Weapon(localPlayer->getWeaponHandle());
-			bool semiauto = m_localWeapon->isSemiAuto();
+			bool semiauto = localWeapon->isSemiAuto();
 			if (counter % 1000 == 0)
 				printf("semiauto is [%d]. \n", semiauto);
 			if (configLoader->isAimbotOn())
