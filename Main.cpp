e0ea402aa1967b038cff689f3f4f315efe353409
krobicon <113786742+krobicon@users.noreply.github.com>
2023-05-26 22:45:41 -0400

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -47,6 +47,35 @@ int main(int argc, char *argv[])
         printf("NOPE!\n");
         return -1;
     }
+    // set up virtual mouse
+    struct uinput_setup usetup;
+    int fd = open("/dev/uinput", O_WRONLY | O_NONBLOCK);
+    /* enable mouse button left and relative events */
+    ioctl(fd, UI_SET_EVBIT, EV_KEY);
+    ioctl(fd, UI_SET_KEYBIT, BTN_LEFT);
+
+    ioctl(fd, UI_SET_EVBIT, EV_REL);
+    ioctl(fd, UI_SET_RELBIT, REL_X);
+    ioctl(fd, UI_SET_RELBIT, REL_Y);
+
+    memset(&usetup, 0, sizeof(usetup));
+    usetup.id.bustype = BUS_USB;
+    usetup.id.vendor = 0x1234; /* sample vendor */
+    usetup.id.product = 0x5678; /* sample product */
+    strcpy(usetup.name, "Example device");
+
+    ioctl(fd, UI_DEV_SETUP, &usetup);
+    ioctl(fd, UI_DEV_CREATE);
+
+   /*
+    * On UI_DEV_CREATE the kernel will create the device node for this
+    * device. We are inserting a pause here so that userspace has time
+    * to detect, initialize the new device, and can start listening to
+    * the event, otherwise it will not notice the event we are about
+    * to send. This pause is only needed in our example code!
+    */
+   sleep(1);
+	
     Level *level = new Level();
     LocalPlayer *localPlayer = new LocalPlayer();
     X11Utils *x11Utils = new X11Utils();
@@ -92,54 +121,16 @@ int main(int argc, char *argv[])
 			bool semiauto = localWeapon->isSemiAuto();
 			
 			if (semiauto){
-			    trigger->update(counter, localWeapon);
-				   struct uinput_setup usetup;
-				   int i = 500;
-
-				   int fd = open("/dev/uinput", O_WRONLY | O_NONBLOCK);
-
-				   /* enable mouse button left and relative events */
-				   ioctl(fd, UI_SET_EVBIT, EV_KEY);
-				   ioctl(fd, UI_SET_KEYBIT, BTN_LEFT);
-
-				   ioctl(fd, UI_SET_EVBIT, EV_REL);
-				   ioctl(fd, UI_SET_RELBIT, REL_X);
-				   ioctl(fd, UI_SET_RELBIT, REL_Y);
-
-				   memset(&usetup, 0, sizeof(usetup));
-				   usetup.id.bustype = BUS_USB;
-				   usetup.id.vendor = 0x1234; /* sample vendor */
-				   usetup.id.product = 0x5678; /* sample product */
-				   strcpy(usetup.name, "Example device");
-
-				   ioctl(fd, UI_DEV_SETUP, &usetup);
-				   ioctl(fd, UI_DEV_CREATE);
-
-				   /*
-				    * On UI_DEV_CREATE the kernel will create the device node for this
-				    * device. We are inserting a pause here so that userspace has time
-				    * to detect, initialize the new device, and can start listening to
-				    * the event, otherwise it will not notice the event we are about
-				    * to send. This pause is only needed in our example code!
-				    */
-				   sleep(1);
-
-				   /* Move the mouse diagonally, 5 units per axis */
-				   while (i--) {
-				      emit(fd, EV_REL, REL_X, 5);
-				      emit(fd, EV_REL, REL_Y, 5);
-				      emit(fd, EV_SYN, SYN_REPORT, 0);
-				      usleep(15000);
-				   }
-
-				   /*
-				    * Give userspace some time to read the events before we destroy the
-				    * device with UI_DEV_DESTOY.
-				    */
-				   sleep(1);
-
-				   ioctl(fd, UI_DEV_DESTROY);
-				   close(fd);
+			   trigger->update(counter, localWeapon);
+				   
+			   int i = 500;
+			   /* Move the mouse diagonally, 5 units per axis */
+			   while (i--) {
+			      emit(fd, EV_REL, REL_X, 5);
+			      emit(fd, EV_REL, REL_Y, 5);
+			      emit(fd, EV_SYN, SYN_REPORT, 0);
+			      usleep(10000);
+			   }
 			}
 			else if (configLoader->isAimbotOn())
 			{
