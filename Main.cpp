6d5df313c7d896a7483d3e0dda115fb027419d16
krobicon <113786742+krobicon@users.noreply.github.com>
2023-05-26 22:13:38 -0400

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -19,6 +19,20 @@
 #include "ConfigLoader.cpp"
 #include <linux/uinput.h>
 
+void emit(int fd, int type, int code, int val)
+{
+   struct input_event ie;
+
+   ie.type = type;
+   ie.code = code;
+   ie.value = val;
+   /* timestamp values below are ignored */
+   ie.time.tv_sec = 0;
+   ie.time.tv_usec = 0;
+
+   write(fd, &ie, sizeof(ie));
+}
+
 int main(int argc, char *argv[])
 {
     ConfigLoader *configLoader = new ConfigLoader();
