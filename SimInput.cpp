29bc7d241244ae5af5088cffd4b4735e71bccf34
krobicon <113786742+krobicon@users.noreply.github.com>
2023-05-27 17:59:55 -0400

    Update SimInput.cpp

--- SimInput.cpp
+++ SimInput.cpp
@@ -5,16 +5,41 @@
 #include <fcntl.h>
 #include <linux/uinput.h>
 
-void emit(int fd, int type, int code, int val)
+class SimInput
 {
-   struct input_event ie;
+   private:
+       struct uinput_setup usetup;
+       int fd = open("/dev/uinput", O_WRONLY | O_NONBLOCK);
+   
+   public:
+       void setup(){
+          ioctl(fd, UI_SET_EVBIT, EV_KEY);
+          ioctl(fd, UI_SET_KEYBIT, BTN_LEFT);
 
-   ie.type = type;
-   ie.code = code;
-   ie.value = val;
-   /* timestamp values below are ignored */
-   ie.time.tv_sec = 0;
-   ie.time.tv_usec = 0;
+          ioctl(fd, UI_SET_EVBIT, EV_REL);
+          ioctl(fd, UI_SET_RELBIT, REL_X);
+          ioctl(fd, UI_SET_RELBIT, REL_Y);
+          
+          memset(&usetup, 0, sizeof(usetup));
+          usetup.id.bustype = BUS_USB;
+          usetup.id.vendor = 0x1234; /* sample vendor */
+          usetup.id.product = 0x5678; /* sample product */
+          strcpy(usetup.name, "Example device");
 
-   write(fd, &ie, sizeof(ie));
+          ioctl(fd, UI_DEV_SETUP, &usetup);
+          ioctl(fd, UI_DEV_CREATE);
+       }
+      void emit(int fd, int type, int code, int val)
+      {
+         struct input_event ie;
+
+         ie.type = type;
+         ie.code = code;
+         ie.value = val;
+         /* timestamp values below are ignored */
+         ie.time.tv_sec = 0;
+         ie.time.tv_usec = 0;
+
+         write(fd, &ie, sizeof(ie));
+      }
 }
