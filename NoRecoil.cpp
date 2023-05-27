8cd569b530316ca08eb531ede52e0867aaaee309
krobicon <113786742+krobicon@users.noreply.github.com>
2023-05-27 19:22:02 -0400

    Update NoRecoil.cpp

--- NoRecoil.cpp
+++ NoRecoil.cpp
@@ -40,7 +40,7 @@ public:
             const double pitch = m_localPlayer->getPitch();
             const double punchPitchDelta = (punchPitch - m_previousPunchPitch);
             printf("punch pitch DELTA: %f \n", punchPitchDelta);
-            if (punchPitchDelta >= 0.0424){
+            if (punchPitchDelta <= 0.0424){
 		    m_simInput->emit(EV_REL, REL_X, 0);
 		    m_simInput->emit(EV_REL, REL_Y, 10);
 		    m_simInput->emit(EV_SYN, SYN_REPORT, 0);
