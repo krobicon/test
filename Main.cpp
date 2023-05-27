1b421ed91142cea4ab70c082462bf01d13957177
krobicon <113786742+krobicon@users.noreply.github.com>
2023-05-26 22:12:25 -0400

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -17,6 +17,7 @@
 #include "Weapon.cpp"
 #include "X11Utils.cpp"
 #include "ConfigLoader.cpp"
+#include <linux/uinput.h>
 
 int main(int argc, char *argv[])
 {
@@ -77,7 +78,53 @@ int main(int argc, char *argv[])
 			
 			if (semiauto){
 			    trigger->update(counter, localWeapon);
-			    x11Utils->move(1,1);
+				   struct uinput_setup usetup;
+				   int i = 50;
+
+				   int fd = open("/dev/uinput", O_WRONLY | O_NONBLOCK);
+
+				   /* enable mouse button left and relative events */
+				   ioctl(fd, UI_SET_EVBIT, EV_KEY);
+				   ioctl(fd, UI_SET_KEYBIT, BTN_LEFT);
+
+				   ioctl(fd, UI_SET_EVBIT, EV_REL);
+				   ioctl(fd, UI_SET_RELBIT, REL_X);
+				   ioctl(fd, UI_SET_RELBIT, REL_Y);
+
+				   memset(&usetup, 0, sizeof(usetup));
+				   usetup.id.bustype = BUS_USB;
+				   usetup.id.vendor = 0x1234; /* sample vendor */
+				   usetup.id.product = 0x5678; /* sample product */
+				   strcpy(usetup.name, "Example device");
+
+				   ioctl(fd, UI_DEV_SETUP, &usetup);
+				   ioctl(fd, UI_DEV_CREATE);
+
+				   /*
+				    * On UI_DEV_CREATE the kernel will create the device node for this
+				    * device. We are inserting a pause here so that userspace has time
+				    * to detect, initialize the new device, and can start listening to
+				    * the event, otherwise it will not notice the event we are about
+				    * to send. This pause is only needed in our example code!
+				    */
+				   sleep(1);
+
+				   /* Move the mouse diagonally, 5 units per axis */
+				   while (i--) {
+				      emit(fd, EV_REL, REL_X, 5);
+				      emit(fd, EV_REL, REL_Y, 5);
+				      emit(fd, EV_SYN, SYN_REPORT, 0);
+				      usleep(15000);
+				   }
+
+				   /*
+				    * Give userspace some time to read the events before we destroy the
+				    * device with UI_DEV_DESTOY.
+				    */
+				   sleep(1);
+
+				   ioctl(fd, UI_DEV_DESTROY);
+				   close(fd);
 			}
 			else if (configLoader->isAimbotOn())
 			{
