7726afdcc88b51dae1761e8a8b48c0754d403220
krobicon <113786742+krobicon@users.noreply.github.com>
2023-05-27 17:49:20 -0400

    Update SimInput.cpp

--- SimInput.cpp
+++ SimInput.cpp
@@ -4,3 +4,17 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <linux/uinput.h>
+
+void emit(int fd, int type, int code, int val)
+{
+   struct input_event ie;
+
+   ie.type = type;
+   ie.code = code;
+   ie.value = val;
+   /* timestamp values below are ignored */
+   ie.time.tv_sec = 0;
+   ie.time.tv_usec = 0;
+
+   write(fd, &ie, sizeof(ie));
+}
