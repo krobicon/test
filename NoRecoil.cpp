c392c761a4860235f6136f8d9255d539c7a31fb6
krobicon <113786742+krobicon@users.noreply.github.com>
2023-05-28 17:30:14 -0400

    Update NoRecoil.cpp

--- NoRecoil.cpp
+++ NoRecoil.cpp
@@ -64,7 +64,7 @@ public:
             //m_localPlayer->setYaw(yaw - punchYawDelta);
             m_previousPunchYaw = punchYaw;
         }
-	if (punchpixPitch != 0) {
+	if (punchpixPitch > 0) {
 		m_simInput->emit(EV_REL, REL_Y, punchpixPitch);
 		m_simInput->emit(EV_REL, REL_X, 0);
 		m_simInput->emit(EV_SYN, SYN_REPORT, 0);
