e90dbe7ff14f1962b954770515bef8963fa37f5d
krobicon <113786742+krobicon@users.noreply.github.com>
2023-05-27 21:14:24 -0400

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -84,7 +84,7 @@ int main(int argc, char *argv[])
 			else if (configLoader->isAimbotOn()){
 			    aimbot->update(counter);
 			}
-			else if (counter % 3 == 0 && localPlayer->isInAttack()){
+			else if (counter % 2 == 0 && localPlayer->isInAttack()){
 			    noRecoil->update();
 			}
 
