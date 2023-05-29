93ccefe7c5b38bb73057f061b6b26aa872182a1d
krobicon <113786742+krobicon@users.noreply.github.com>
2023-05-29 16:38:11 -0400

    Update Farming.cpp

--- Farming.cpp
+++ Farming.cpp
@@ -58,8 +58,8 @@ public:
 		if (counter % 2500 == 0 && m_localPlayer->isGrounded())
 			m_simInput->click();
 	}
-	else if (m_localPlayer->isDead() && counter % 1000 == 0) {
-					        int k = 0;
+	else if (m_localPlayer->isDead() && counter % 1000 == 0 && findMatch == false) {
+				 int k = 0;
 			    	int j = 0;
 			    	int p = 0;
 				m_simInput->emit(EV_KEY, KEY_SPACE, 1);
