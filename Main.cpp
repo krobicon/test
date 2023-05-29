faad420a03abf134b6b1db1f1ea2ef6cdf8aebfd
krobicon <113786742+krobicon@users.noreply.github.com>
2023-05-29 00:07:16 -0400

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -122,16 +122,16 @@ int main(int argc, char *argv[])
 			    	simInput->click();
 			    	usleep(10000);
 			    	while (p < 30) {
-					simInput->emit(EV_REL, REL_Y, 7);
+					simInput->emit(EV_REL, REL_Y, 8);
 					simInput->emit(EV_REL, REL_X, 2);
 					simInput->emit(EV_SYN, SYN_REPORT, 0);
 					p++;
 					usleep(10000);
 				}
 			    	simInput->click();
-			    	usleep(100000);
+			    	usleep(200000);
 			    	simInput->click();
-			    	usleep(100000);
+			    	usleep(200000);
 			    	simInput->click();
 		    }
             // run features
