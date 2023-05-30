e214ca6a9ef40e01e039b4b24a52996741175921
krobicon <113786742+krobicon@users.noreply.github.com>
2023-05-29 23:38:47 -0400

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -133,7 +133,7 @@ int main(int argc, char *argv[])
         catch (...)
         {
             printf("SLEEPING[%d]\n", counter);
-            std::this_thread::sleep_for(std::chrono::seconds(9));
+            std::this_thread::sleep_for(std::chrono::seconds(10));
         }
         counter++;
     }
