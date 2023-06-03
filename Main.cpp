4666a0d509c2ebb4237e547dbf272eb9f14d1222
krobicon <113786742+krobicon@users.noreply.github.com>
2023-06-03 13:06:46 -0400

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -87,7 +87,7 @@ int main(int argc, char *argv[])
 			Weapon *localWeapon = new Weapon(localPlayer->getWeaponHandle());
 			bool triggergun = localWeapon->isSemiAuto() && !localWeapon->isBurst();
 			if (counter % 10 == 0) {
-				printf("TS?: %f\n", localPlayer->getTraversalStart());
+				printf("hang time?: %f\n", localPlayer->getTime() - localPlayer->getTraversalStart());
 				printf("TP?: %f\n", localPlayer->getTraversalProgress());
 			}
 			if (triggergun) {
