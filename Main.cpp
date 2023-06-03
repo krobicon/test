ad5be0a3a5a53a83ff9a90b22f7ae3c79bb5c1ce
krobicon <113786742+krobicon@users.noreply.github.com>
2023-06-03 14:41:49 -0400

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -86,12 +86,12 @@ int main(int argc, char *argv[])
 			movement->update();
 			Weapon *localWeapon = new Weapon(localPlayer->getWeaponHandle());
 			bool triggergun = localWeapon->isSemiAuto() && !localWeapon->isBurst();
-			if (counter % 10 == 0) {
+			/*if (counter % 10 == 0) {
 				//printf("is climbing?: %d\n", localPlayer->isClimbing());
 				if (localPlayer->isClimbing())
 					printf("climb time? %f\n", localPlayer->getTime() - localPlayer->getWallrunStart());
 				//printf("wall clear?: %f\n", localPlayer->getWallrunClear());
-			}
+			}*/
 			if (triggergun) {
 			   trigger->update(counter, localWeapon);
 			}
