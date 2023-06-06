63ce8f042f70d9c0605c843d039eb2bd943c9913
krobicon <113786742+krobicon@users.noreply.github.com>
2023-06-05 20:24:05 -0400

    Update Movement.cpp

--- Movement.cpp
+++ Movement.cpp
@@ -106,7 +106,7 @@ public:
 		}
 		printf("how many times? \n");
 	}
-	else if ((worldTime - startjumpTime) >= 0.8f && startSg){
+	else if ((worldTime - startjumpTime) >= 0.5f && startSg){
 	//need to release button
 		m_localPlayer->setJumpState(4);
 		if (m_localPlayer->getDuckDown() != 79) {
