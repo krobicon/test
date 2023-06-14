5f4d6e50cac9b4aa7d996c0381b8861de82c0dfa
krobicon <113786742+krobicon@users.noreply.github.com>
2023-06-14 12:43:59 -0400

    Update Movement.cpp

--- Movement.cpp
+++ Movement.cpp
@@ -51,13 +51,13 @@ public:
 			longclimb = false;
 	}
 	// auto tap trafe
-	if (!m_localPlayer->isGrounded() && !m_localPlayer->isSkydiving() && !longclimb && !m_localPlayer->getJumpDown())
+	if (!m_localPlayer->isGrounded() && !m_localPlayer->isSkydiving() && !longclimb)
 	{
 		if (jumpstart == false) {
 			jumpstart = true;
 			strafeTick = 0;
 		}
-		else if (m_localPlayer->isDucking() || (strafeTick > 20 && strafeTick < 130 && m_localPlayer->getForwardDown() == 33)) { //previously 33
+		else if (!m_localPlayer->getJumpDown() && (m_localPlayer->isDucking() || (strafeTick > 20 && strafeTick < 130 && m_localPlayer->getForwardDown() == 33))) { //previously 33
 			if (m_localPlayer->getForwardState() == 0) {
 				m_localPlayer->setForwardState(5);
 				//printf("Forward State set:[%d] \n", m_localPlayer->getForwardState());
