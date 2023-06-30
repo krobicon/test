308c876d861a5381faca8e814d62827390db9470
krobicon <113786742+krobicon@users.noreply.github.com>
2023-06-30 13:26:46 -0400

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -83,14 +83,12 @@ int main(int argc, char *argv[])
 			movement->update(counter);
 			Weapon *localWeapon = new Weapon(localPlayer->getWeaponHandle());
 			bool triggergun = localWeapon->isSemiAuto() && !localWeapon->isBurst();
-			if (counter % 500 == 0) {
-				   simInput->space();
-
+			//if (counter % 500 == 0) {
 				//printf("is climbing?: %d\n", localPlayer->isClimbing());
 				//if (localPlayer->isClimbing())
 					//printf("climb time? %f\n", localPlayer->getTime() - localPlayer->getWallrunStart());
 				//printf("semiauto?: %d\n", localWeapon->isSemiAuto());
-			}
+			//}
 			if (configLoader->isAimbotOn()){
 				if (counter % 30 == 0) {
 					if (rand() % 5 == 0) {
