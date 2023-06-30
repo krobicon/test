c6b49a7f62f695aa49cbc6890e3ca62b5b471bce
krobicon <113786742+krobicon@users.noreply.github.com>
2023-06-29 20:20:20 -0400

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -96,7 +96,7 @@ int main(int argc, char *argv[])
 						printf("switch \n");
 					}
 				}
-			    if (aimSwitch && rand() % 6 == 0)
+			    if (aimSwitch && rand() % 5 == 0)
 			    	aimbot->update(counter);
 			}
 			if (triggergun) {
