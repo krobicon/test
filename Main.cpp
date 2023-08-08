fdfa3eacbdbd803f04fa25118fda94b2a28e7ede
krobicon <113786742+krobicon@users.noreply.github.com>
2023-08-08 17:57:36 -0400

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -70,26 +70,17 @@ int main(int argc, char *argv[])
                 Player *player = players->at(i);
                 player->markForPointerResolution();
             }
-
-		printf("SUCCESS 1\n");
 	    
 	    if (configLoader->isFarmingOn()) {
 		    farming->update(counter);
 	    }
-
-		printf("SUCCESS 2\n");
 		
 	    if (level->isPlayable()) {
             	// run features
-		    printf("SUCCESS 3\n");
-		//sense->update(counter);
-
-		    printf("SUCCESS 4\n");
+		sense->update(counter);
 		    
 		if (!localPlayer->isDead() && !localPlayer->isKnocked()) {
-			printf("SUCCESS 5\n");
 			movement->update(counter);
-			printf("SUCCESS 6\n");
 			Weapon *localWeapon = new Weapon(localPlayer->getWeaponHandle());
 			bool triggergun = localWeapon->isSemiAuto() && !localWeapon->isBurst();
 			//if (counter % 100 == 0) {
